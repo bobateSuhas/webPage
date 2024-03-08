da037ff5c84ba989e42a7cbc794fb308f0ec066b
bobateSuhas <suhasbobate@gmail.com>
2024-03-09 00:42:21 +0530

    empployee file of java

---
+++ Employee.java
@@ -0,0 +1,20 @@
+
+
+import java util.*;
+public class Employee
+{
+public static void main(string []);
+{
+scanner E=new scanner(system.in);
+long salary;
+char name[100];
+int id;
+system.out.printf("enter the name ,id and  salary of the employee\n");
+name=E.nextline();
+salary=E.nextlong();
+id=E.nextint();
+system.out.printf("name=%s",name);
+system.out.printf("salary=%ld",salary);
+system.out.printf("id=%d",id);
+}
+}
